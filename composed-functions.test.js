cd10a0e68146fd4751d404a052c1bf688ac71847
crea00 <654321su@gmail.com>
2018-10-24 10:02:38 +0900

    Session4_Composing_Functions

---
+++ composed-functions.test.js
@@ -0,0 +1,20 @@
+const { compose, add, __, pipe } = require('ramda');
+
+const addFive = add(__, 5);
+const sqr = n => n * n;
+
+test('can compose functions', () => {
+  const five = compose(addFive, sqr);
+  const twentyFive = compose(sqr, addFive)
+
+  expect(five(0)).toBe(5)
+  expect(twentyFive(0)).toBe(25)
+})
+
+test('can pipe functions', () => {
+  const five = pipe(addFive, sqr);
+  const twentyFive = pipe(sqr, addFive)
+
+  expect(five(0)).toBe(25)
+  expect(twentyFive(0)).toBe(5)
+})
