d76f383dc8420e62d745409bec08a6f2da105df9
Jerome <654321su@gmail.com>
2018-10-27 23:24:01 +0900

    Session6_Point-Free

---
+++ point-free.test.js
@@ -0,0 +1,71 @@
+const {
+  pipe,
+  tap,
+  pick,
+  evolve,
+  map,
+  prop,
+  props,
+  zip,
+  join,
+} = require('ramda');
+const data = {
+  coord: { lon: -0.13, lat: 51.51 },
+  weather: 
+  [
+    { id: 803, 
+      main: 'Clouds',
+      description: 'broken clouds',
+      icon: '04d'
+    }
+  ],
+  base: 'stations',
+  main: {
+    temp: 285.06,
+    pressure: 1014,
+    humidity: 76,
+    temp_min: 284.15,
+    temp_max: 286.15
+  },
+  visibility: 10000,
+  wind: {
+    speed: 9.3,
+    deg: 240,
+    gust: 15.4
+  },
+  clouds: { 
+    all: 75
+  },
+  dt: 1511178600,
+  sys: {
+    type: 1,
+    id: 5091,
+    message: 0.0087,
+    country: 'GB',
+    sunrise: 1511162789,
+    sunset: 1511193846
+  },
+  id: 2643743,
+  name: 'London',
+  cod: 200
+};
+
+test.only('point free style', () => {
+  const transformWeather = {
+    weather: pipe(
+      map(prop('description')),
+      join(' ')
+    )
+  }
+  const summarySentence = pipe(
+    pick(['name', 'weather']),
+    evolve(transformWeather),
+    props([ 'name', 'weather']),
+    zip(['Today in', 'you can expect']),
+    map(join(' ')),
+    join(' '),
+    tap(console.log)
+  )
+  expect(summarySentence(data))
+    .toEqual('Today in London you can expect broken clouds')
+})
