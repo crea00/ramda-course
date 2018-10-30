b0237bc670465b47d03a53b4f2a4a5135858d562
Jerome <654321su@gmail.com>
2018-10-31 00:41:43 +0900

    Session10_Monads

--- monad.test.js
+++ monad.test.js
@@ -18,5 +18,37 @@ test('Fantasy Land', () => {
   }
 
   expect(wrappedVal.ap(wrappedFn)).toEqual([5])
+
+  Array.prototype.of = function(val) {
+    return new Array(1).fill(val)
+  }
+
+  expect([].of(2)).toEqual([2])
+
+  Array.prototype.chain = function(fn) {
+    return !this.map(fn)[0] ? this : this.map(fn)
+  }
+
+  expect([3]
+    .chain(add3)
+    .chain(add3)
+    .chain(add3)
+  ).toEqual([12])
+
+  expect([3]
+    .chain(add3)
+    .chain(() => undefined)
+    .chain(add3)
+  ).toEqual([9])
+
+  Array.prototype.just = function() {
+    return this[0]
+  }
+  Array.prototype.nothing = function() {
+    return !this[0]
+  }
+
+  expect([undefined].nothing()).toBe(true)
+  expect([3].just()).toBe(3)
 })
 
